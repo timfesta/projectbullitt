6603d0437cf1919a252c8b6a3a6d5648ed6012ef
Antonio de Leon <tonichi@gmail.com>
2015-08-19 15:06:51 -0700

    added edit & delete functionalities for NPO

--- app/controllers/npos_controller.rb
+++ app/controllers/npos_controller.rb
@@ -24,12 +24,29 @@ class NposController < ApplicationController
 	end
 
 	def edit
-		@npo = Npo.find(current_user)
+		@npo = Npo.find(params[:id])
 		render :edit
 	end
 
-	def update
-	end 
+  def update
+    npo = Npo.find(params[:id])
+    if current_user.npos.include? npo
+        npo.update_attributes(npo_params)
+        redirect_to npo_path(npo)
+    else
+      redirect_to signup_path
+    end
+  end
+
+  def destroy
+    npo = Npo.find(params[:id])
+    if current_user.npos.include? npo
+      npo.destroy
+      redirect_to profile_path(current_user)
+    else
+      redirect_to login_path
+    end
+  end
 
 	private 
 	def npo_params 
