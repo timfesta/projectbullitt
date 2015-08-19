64dd426fb44ccbd42506a7722404e9f3105ff4dd
Antonio de Leon <tonichi@gmail.com>
2015-08-19 11:23:54 -0700

    added user avatar functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
   
   private 
     def user_params 
-      params.require(:user).permit(:username, :email, :password, :is_npo, :created_at)
+      params.require(:user).permit(:username, :email, :password, :is_npo, :created_at, :avatar)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
 	has_many :npos
   has_many :projects
   has_many :tasks, through: :projects
+
+
+# for profile photos
+  has_attached_file :avatar,
+                   :styles => { :medium => "150x150>", :thumb => "44x44" },
+                   :default_url => "/images/:style/missing.png"
+
+                   		# :avatar, :presence => true,
+ validates_attachment :avatar,
+                      :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] },
+                      :size => { :in => 0..100000.kilobytes }
+
+
 end
